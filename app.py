0c41768a5e8f716ea6c66743bf5f68aa2a1f5f2b
Xhoni Shollaj <xshollaj001@cee.ema.ad.pwcinternal.com>
2020-05-27 12:04:09 +0200

    First commit

---
+++ app.py
@@ -0,0 +1,105 @@
+from __future__ import unicode_literals
+import os
+from flask import Flask, render_template, request
+from flask_uploads import UploadSet, configure_uploads, ALL, DATA
+from werkzeug.utils import secure_filename
+from flask import Flask,render_template,url_for,request
+from spacy_summarization import text_summarizer
+from gensim.summarization import summarize
+import textract
+import time
+import spacy
+nlp = spacy.load("en_core_web_lg")
+
+app = Flask(__name__)
+
+#Configuration
+files = UploadSet('files',ALL)
+app.config['UPLOADED_FILES_DEST'] = 'static/uploads'
+configure_uploads(app,files)
+
+path = os.getcwd()
+
+# Web Scraping Pkg
+from bs4 import BeautifulSoup
+# from urllib.request import urlopen
+from urllib.request import urlopen
+
+# Sumy Pkg
+from sumy.parsers.plaintext import PlaintextParser
+from sumy.nlp.tokenizers import Tokenizer
+from sumy.summarizers.lex_rank import LexRankSummarizer
+
+
+
+	
+# Sumy 
+def sumy_summary(docx):
+	parser = PlaintextParser.from_string(docx,Tokenizer("english"))
+	lex_summarizer = LexRankSummarizer()
+	summary = lex_summarizer(parser.document,3)
+	summary_list = [str(sentence) for sentence in summary]
+	result = ' '.join(summary_list)
+	return result
+
+
+# Reading Time
+def readingTime(mytext):
+	total_words = len([ token.text for token in nlp(mytext)])
+	estimatedTime = total_words/200.0
+	return estimatedTime
+
+# Fetch Text From Url
+def get_text(url):
+	page = urlopen(url)
+	soup = BeautifulSoup(page)
+	fetched_text = ' '.join(map(lambda p:p.text,soup.find_all('p')))
+	return fetched_text
+
+@app.route('/')
+def index():
+	return render_template('index.html')
+
+@app.route('/datauploads')
+def datauploads():
+	return render_template('index.html',methods)
+
+
+
+
+@app.route('/analyze',methods=['GET','POST'])
+def analyze():
+	start = time.time()
+	if request.method == 'POST':
+		rawtext = request.form['rawtext']
+		final_reading_time = readingTime(rawtext)
+		final_summary = text_summarizer(rawtext)
+		summary_reading_time = readingTime(final_summary)
+		end = time.time()
+		final_time = end-start
+	return render_template('index.html',ctext=rawtext,final_summary=final_summary,final_time=final_time,final_reading_time=final_reading_time,summary_reading_time=summary_reading_time)
+
+@app.route('/analyze_url',methods=['GET','POST'])
+def analyze_url():
+	start = time.time()
+	if request.method == 'POST':
+		raw_url = request.form['raw_url']
+		rawtext = get_text(raw_url)
+		final_reading_time = readingTime(rawtext)
+		final_summary = text_summarizer(rawtext)
+		summary_reading_time = readingTime(final_summary)
+		end = time.time()
+		final_time = end-start
+	return render_template('index.html',ctext=rawtext,final_summary=final_summary,final_time=final_time,final_reading_time=final_reading_time,summary_reading_time=summary_reading_time)
+
+   
+	
+@app.route('/uploader', methods = ['GET', 'POST'])
+def upload_file():
+	if request.method == 'POST':
+		f = request.files['file']
+		f.save(secure_filename(f.filename))
+		return 'file uploaded successfully'
+
+if __name__ == '__main__':
+	app.run(debug=True)
